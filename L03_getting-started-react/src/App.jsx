c95c4bbed2985fed58e39990d7d181063f0e4c10
Winston Gultiano <wgultiano08@gmail.com>
2026-07-18 16:26:57 +0800

    Done with laboratory 3

---
+++ L03_getting-started-react/src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <>
+      <h1>About Me:</h1>
+      <p>Winston Gultiano</p>
+      <p>BSIS - 3</p>
+      <ol>
+        <li>I am always willing to learn from my mistakes.</li>
+      </ol>
+      <hr />
+      <p>In the past session, we learned about SSH and HTTPS and how to use them in different case or scenario.</p>
+      <p>For example, if you have your own device or computer you can use SSH or Secure Shell for you to access your own device.</p>
+      <p>While HTTPS or Hypertext Transfer Protocol Secure is used when you want to access a website using your credentials.</p>
+      <p>Also, we learned some Git commands that will help us to collaborate with others.</p>
+      <p>Today, we had a discussion about React and do a laboratory. </p>
+    </>
+  )
+}
+
+export default App
